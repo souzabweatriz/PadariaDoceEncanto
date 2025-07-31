8e0f94c540ce0f2b91c03005949ecc6ea68072c5
Isabella <isabella.tds1.senai@gmail.com>
2025-07-31 09:24:29 -0300

    feat: add content

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import Tab from "./navigation/TabNavigation";
+import StackNavigator from "./navigation/StackNavigator";
 
 export default function App() {
     return (
         <NavigationContainer>
-            <Tab />
+            <StackNavigator />
         </NavigationContainer>
     );
 }
--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -1,6 +1,6 @@
 import React from "react"; 
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"; 
-import Home from "../pages/Home";  
+import Home from "../screens/Home";  
 import Icon from "react-native-vector-icons/Ionicons";
 
 const Tab = createBottomTabNavigator(); 
